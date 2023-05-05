8b201275f2a94878a2d7158c1ce79452bdf7391f
Luis Oliveira <luiolive3@publicisgroupe.net>
2023-05-05 12:38:00 +0100

    Initial commit

---
+++ spark_vs_polars/polars_performance.py
@@ -0,0 +1,108 @@
+import polars as pl
+import time
+
+def extraction():
+    path1="yellow_tripdata.parquet"
+    df_trips= pl_read_parquet(path1,)
+    path2 = "taxi+_zone_lookup.parquet"
+    df_zone = pl_read_parquet(path2,)
+
+    return df_trips, df_zone
+
+def pl_read_parquet(path, ):
+    """
+    Converting parquet file into Polars dataframe
+    """
+    df= pl.read_parquet(path,)
+    return df
+
+def transformation(df_trips, df_zone):
+    df_trips= mean_test_speed_pl(df_trips, )
+    df = df_trips.join(df_zone,how="inner", left_on="PULocationID", right_on="LocationID",)
+    df = df[["Borough","Zone","trip_distance","passenger_count"]]
+  
+    df = get_Queens_test_speed_pd(df)
+    df = round_column(df, "passenger_count",0)
+    df = round_column(df, "trip_distance",2)
+    
+    df = rename_column(df, "passenger_count","mean_passenger_count")
+    df = rename_column(df, "trip_distance","mean_trip_distance")
+    df = sort_by_columns_desc(df, "mean_passenger_count")
+    return df
+
+def rename_column(df, column_old, column_new):
+    """
+    Renaming columns
+    """
+    df = df.rename({column_old: column_new})
+    return df
+
+def mean_test_speed_pl(df_pl,):
+    """
+    Getting Mean per PULocationID
+    """
+    df_pl = df_pl[['PULocationID', "trip_distance", "passenger_count"]].groupby('PULocationID').mean()
+    return df_pl
+
+def sort_by_columns_desc(df, column):
+    """
+    Sort by column
+    """
+    df = df.sort(column, descending=True)
+    return df
+
+def round_column(df, column,to_round):
+    """
+    Round numbers on columns
+    """
+    df = df.with_columns(pl.col(column).round(to_round))
+    return df
+
+def get_Queens_test_speed_pd(df_pl):
+    """
+    Only getting Borough in Queens
+    """
+
+    df_pl = df_pl.filter(pl.col("Borough")=='Queens')
+
+    return df_pl
+
+def loading_into_parquet(df_pl):
+    """
+    Save dataframe in parquet
+    """
+    df_pl.write_parquet(f'yellow_tripdata_pl.parquet')
+
+def main():
+    
+    print(f'Starting ETL for Polars')
+    print("\n")
+    start_time = time.perf_counter()
+
+    print('Extracting...')
+    df_trips, df_zone =extraction()
+       
+    end_extract=time.perf_counter() 
+    time_extract =end_extract- start_time
+
+    print(f'Extraction Parquet end in {round(time_extract,3)} seconds')
+    
+    print("\n")
+    print('Transforming...')
+    df = transformation(df_trips, df_zone)
+    end_transform = time.perf_counter() 
+    time_transformation =time.perf_counter() - end_extract
+    print(f'Transformation end in {round(time_transformation,3)} seconds')
+    
+    print("\n")
+    print('Loading...')
+    loading_into_parquet(df,)
+    load_transformation =time.perf_counter() - end_transform
+    print(f'Loading end in {round(load_transformation,3)} seconds')
+    print("\n")
+    print(f"End ETL for Polars in {str(time.perf_counter()-start_time)}")
+
+
+if __name__ == "__main__":
+    
+    main()
---
+++ spark_vs_polars/pyspark_performance.py
@@ -0,0 +1,86 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import mean, col, round
+import time
+
+def round_column(df, column,to_round):
+    """
+    Round numbers on columns
+    """
+    df = df.withColumn(column, round(col(column), to_round))
+
+    return df
+
+def get_Queens_test_speed_py(df):
+    """
+    Only getting Borough in Queens
+    """
+    df = df.filter(col("Borough") == "Queens")
+    return df
+
+def mean_test_speed_py(df,):
+    """
+    Getting Mean per PULocationID
+    """
+    df = df.groupby("PULocationID").agg(mean("trip_distance").alias("mean_trip_distance"), mean("passenger_count").alias("mean_passenger_count"))
+
+    return df
+
+
+def extraction():
+    spark = SparkSession.builder.appName("ETL").getOrCreate()
+    path1 = "yellow_tripdata.parquet"
+    df_trips = spark.read.parquet(path1)
+    path2 = "taxi+_zone_lookup.parquet"
+    df_zone = spark.read.parquet(path2)
+    return df_trips, df_zone
+
+def transformation(df_trips, df_zone):
+    # Get Mean
+    df_trips=mean_test_speed_py(df_trips)
+
+    df = df_trips.join(df_zone, col("PULocationID") == col("LocationID"), "inner")
+    df = df.select(col("Borough"), col("Zone"), col("mean_trip_distance"), col("mean_passenger_count"))
+    
+    # Get one Borough
+    df= get_Queens_test_speed_py(df)
+    # Round columns
+    df = round_column(df,"mean_passenger_count", 0)
+    df = round_column(df,"mean_trip_distance", 2)
+
+    df = df.orderBy(col("mean_passenger_count").desc())
+    return df
+
+def loading_into_parquet(df):
+    df.write.parquet("yellow_tripdata_pyspark", mode="overwrite")
+
+def main():
+    print("Starting ETL for PySpark")
+    print("\n")
+    start_time = time.perf_counter()
+
+    print("Extracting...")
+    df_trips, df_zone = extraction()
+
+    end_extract = time.perf_counter()
+    time_extract = end_extract - start_time
+    print(f"Extraction Parquet end in {str(time_extract)} seconds")
+
+    print("\n")
+    print("Transforming...")
+    df = transformation(df_trips, df_zone)
+    end_transform = time.perf_counter()
+    time_transformation = end_transform - end_extract
+    print(f"Transformation end in {str(time_transformation)} seconds")
+
+    print("\n")
+    print("Loading...")
+    loading_into_parquet(df)
+    load_transformation = time.perf_counter() - end_transform
+    print(f"Loading end in {str(load_transformation)} seconds")
+    print("\n")
+    print(f"End ETL for PySpark in {str(time.perf_counter()-start_time)}")
+
+
+if __name__ == "__main__":
+    
+    main()
